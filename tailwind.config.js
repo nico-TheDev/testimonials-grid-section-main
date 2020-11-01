a9501bed3616d44c079c984107927c28cca58ed6
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-11-01 13:35:26 +0800

    add style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
+    purge: ["index.html"],
+    theme: {
+        extend: {
+            colors: {
+                moderateViolet: "hsl(263, 55%, 52%)",
+                grayishBlue: "hsl(217, 19%, 35%)",
+                blackBlue: "hsl(219, 29%, 14%)",
+                lightGray: "hsl(0, 0%, 81%)",
+                lightGrayBlue: "hsl(210, 46%, 95%)",
+            },
+            fontSize: {
+                default: "13px",
+            },
+            fontFamily: {
+                barlow: ["Barlow Semi Condensed", "sans-serif"],
+                arial:["Times New Roman","sans-serif"]
+            },
+            height:{
+                60:"60vh",
+                70:"70vh",
+                80:"80vh",
+                90:"90vh",
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
+};
